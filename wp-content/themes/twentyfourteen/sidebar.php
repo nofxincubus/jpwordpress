6a03c0d054c7e2a454535e89accb1dae78ac7701
Calvin Park <nofxincubus@gmail.com>
2014-02-03 23:36:57 -0500

    removed sidebar

--- wp-content/themes/twentyfourteen/sidebar.php
+++ wp-content/themes/twentyfourteen/sidebar.php
@@ -21,9 +21,5 @@
 	</nav>
 	<?php endif; ?>
 
-	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-	<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	</div><!-- #primary-sidebar -->
-	<?php endif; ?>
+	
 </div><!-- #secondary -->
